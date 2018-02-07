e5eeb9e667eaf09d49399e22102c7eaf3176b943
afrancocaro <alejandro.franco.caro@gmail.com>
2018-02-07 07:54:38 +0100

    Create nveces_01.py

---
+++ Bucles While/nveces_01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*-coding: utf-8-*-
+
+primero = int(input("Escriba un número: "))
+segundo = int(input("Escriba un número mayor que " + str(primero) + ": "))
+
+while segundo <= primero:
+    print(segundo, "no es mayor que", str(primero) + ". Inténtelo de nuevo: ",
+          end="")
+    segundo = int(input())
+
+print()
+print("Los números que ha escrito son", primero, "y", str(segundo) + ".")
+print("Programa terminado")
